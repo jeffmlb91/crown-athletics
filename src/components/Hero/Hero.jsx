834ce88a290fd45313b330c58cc23ec7bb93094b
jeffmlb91 <66559059+jeffmlb91@users.noreply.github.com>
2025-02-09 15:57:46 -0500

    hero: completed section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './Hero.css'
 import Header from './Header/Header'
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Hearts from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
 
 const Hero = () => {
   return (
@@ -53,7 +57,31 @@ const Hero = () => {
             <buttons className="btn">Learn More</buttons>
         </div>
     </div>
-    <div className="right-h">Right side</div>
+
+    {/**Right Side */}
+    <div className="right-h">
+        <button className='btn'>Join Now</button>
+
+        <div className="heart-rate">
+            <img src={Hearts} alt="" />
+            <span>Heart Rate</span>
+            <span>116 bpm</span>
+        </div>
+
+        {/**images */}
+        <img src={hero_image} alt="" className='hero-image'/>
+        <img src={hero_image_back} alt="" className='hero-image-back' />
+
+        {/**Calories */}
+
+        <div className="calories">
+            <img src={Calories} alt="" />
+            <div>
+                <span>Calories Burned</span>
+                <span>220 Kcal</span>
+            </div>
+        </div>
+    </div>
    </div>
   )
 }
